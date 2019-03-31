68902acab3fecdcc16ad52e54dde7ab60cf74d4c
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-03-31 22:31:20 +0200

    Adding second test for IBAN validation in the controller.

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import pytest
 
 from app.controllers import validate_iban
@@ -13,3 +14,15 @@ def test_validate_iban_raises_invalid_iban_error():
 
     with pytest.raises(InvalidIBANError):
         validate_iban(iban)
+
+
+def test_validate_iban_calls_schwifty_iban_function():
+    """
+    Test passing a valid IBAN value to the validate_iban controller will call
+    schwifty IBAN function.
+    """
+    iban = 'DX89 3704 0044 0532 0130 00'
+
+    with patch('app.controllers.IBAN') as PatchedIBAN:
+        validate_iban(iban)
+        PatchedIBAN.assert_called_once_with(iban)
