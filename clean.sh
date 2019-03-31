c1e1bd21ad8c6844aa6e2ae7a4c22255da85e254
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-01 00:06:09 +0200

    Adding the correct message for the script.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Running the IBAN validation API tests"
+echo "Removing the python and pytest cache files"
 set -e
 sudo find . -type d -name __pycache__ | sudo xargs rm -rf
 sudo rm -rf .pytest_cache/
