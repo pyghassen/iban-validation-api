39cbdec6dd471feb65f9ee6ee60c2555455d5419
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-04-01 00:04:31 +0200

    Adding the validate_iban_view.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
 from sanic import Sanic
 from sanic.response import json
 
+from app.controllers import validate_iban
+from app.exceptions import InvalidIBANError
+
 http_server = Sanic()
 
 @http_server.route("/health")
-def health(request):
+def health_view(request):
     return json({"status": "ok"})
+
+
+@http_server.route("/iban/validate/")
+def validate_iban_view(request, iban):
+    try:
+        validate_iban(iban)
+    except InvalidIBANError:
+        return json({"valid_iban": False}, status=400)
+
+    return json({"valid_iban": True})
