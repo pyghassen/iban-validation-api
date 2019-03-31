60900bf76b50d138cd6e785ff7e164aa36c5c5dc
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2019-03-31 23:58:07 +0200

    Adding tests for validate IBAN view with valid data.

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,16 +1,30 @@
 from unittest.mock import Mock
 
-from app.views import health
+from app.views import health_view, validate_iban_view
 
 
-def test_health_returns_expected_message():
+def test_health_returns_expected_success_message():
     """
     Test health view return the expected message.
     """
     expected_message = '{"status":"ok"}'
     mocked_request = Mock()
+    response = health_view(mocked_request)
 
-    response = health(mocked_request)
+    assert response.status == 200
+    assert expected_message == response.body.decode()
+
+
+def test_validate_returns_expected_success_message():
+    """
+    Test health view return the expected message.
+    """
+    expected_message = '{"valid_iban":true}'
+    mocked_request = Mock()
+
+    iban = 'DE89 3704 0044 0532 0130 00'
+
+    response = validate_iban_view(mocked_request, iban)
 
     assert response.status == 200
     assert expected_message == response.body.decode()
